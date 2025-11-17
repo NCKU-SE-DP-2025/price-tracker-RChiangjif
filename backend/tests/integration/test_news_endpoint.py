1c7f69b53e5866d4262560a9bc41be80f9704f7b
RChiangjif <ray2060417@gmail.com>
2025-11-17 23:06:05 +0800

    fix: integration-test

--- backend/tests/integration/test_news_endpoint.py
+++ backend/tests/integration/test_news_endpoint.py
@@ -57,6 +57,10 @@ def test_token(test_user):
 @pytest.fixture(scope="module")
 def test_articles():
     with next(override_session_opener()) as db:
+        # 清理舊資料
+        db.query(NewsArticle).delete()
+        db.commit()
+        
         article_1 = NewsArticle(
             url="https://example.com/test-news-1",
             title="Test News 1",
